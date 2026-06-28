c47c4382bb25ae66286f988ccf012a2e5ca8f7bc
Sagar Kadam <sskadam128@gmail.com>
2026-06-28 23:29:35 +0530

    Created basic ADK project structure

--- 02_bigquery_agent/agent.py
+++ 02_bigquery_agent/agent.py
@@ -1,1 +1,13 @@
+from google.adk.agents import Agent
 
+root_agent = Agent(
+    name="bigquery_agent",
+    model="gemini-2.5-flash",
+    instruction="""
+    You are a helpful AI assistant.
+
+    Answer the user's questions politely.
+
+    If you don't know the answer, say you don't know.
+    """
+)
